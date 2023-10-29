0673064bb33cf4adab5ea4be864dd20a1ddb4562
Svyatoslav Pankov <slavapank94@yandex.ru>
2023-10-29 14:33:12 +0600

    feat: implement Loader

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import { SearchForm } from './components/SearchForm';
 import { Heading } from './components/Heading';
 import { IPerson } from './types/IPerson';
 import { ResultList } from './components/ResultList';
+import { Loader } from './components/Loader';
 
 type IProps = object;
 
 type IState = {
   results: IPerson[];
+  isLoading: boolean;
 };
 
 export class App extends Component<IProps, IState> {
@@ -15,25 +17,27 @@ export class App extends Component<IProps, IState> {
     super(props);
     this.state = {
       results: [],
+      isLoading: false,
     };
   }
 
   handleSubmit = async (search: string) => {
+    this.setState({ isLoading: true });
     const response = await fetch(
       `https://swapi.dev/api/people/?search=${search.trim()}`
     );
     const { results }: { results: IPerson[] } = await response.json();
-    this.setState({ results });
+    this.setState({ results, isLoading: false });
   };
 
   render() {
-    const { results } = this.state;
+    const { results, isLoading } = this.state;
 
     return (
       <>
         <Heading />
         <SearchForm onSubmit={this.handleSubmit} />
-        <ResultList list={results} />
+        {isLoading ? <Loader /> : <ResultList list={results} />}
       </>
     );
   }
