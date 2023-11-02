00fabaac32178ca9d493a70e0806a7dd4808064e
Svyatoslav Pankov <slavapank94@yandex.ru>
2023-11-02 11:59:52 +0600

    refactor: migrate Fallback to functional component

--- src/components/Fallback/Fallback.tsx
+++ src/components/Fallback/Fallback.tsx
@@ -1,15 +1,12 @@
-import { Component } from 'react';
 import './fallback.css';
 
-export class Fallback extends Component<object, object> {
-  render() {
-    return (
-      <div className="fallback">
-        <h1>Oops! Something goes wrong...</h1>
-        <button type="button" onClick={() => window.location.reload()}>
-          Reload
-        </button>
-      </div>
-    );
-  }
+export function Fallback() {
+  return (
+    <div className="fallback">
+      <h1>Oops! Something goes wrong...</h1>
+      <button type="button" onClick={() => window.location.reload()}>
+        Reload
+      </button>
+    </div>
+  );
 }
