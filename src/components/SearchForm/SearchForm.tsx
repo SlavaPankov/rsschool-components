56000ba72159538621adce8ba34b58980e455c3c
Svyatoslav Pankov <slavapank94@yandex.ru>
2023-11-02 12:14:00 +0600

    refactor: migrate SearchForm to functional component

--- src/components/SearchForm/SearchForm.tsx
+++ src/components/SearchForm/SearchForm.tsx
@@ -1,70 +1,49 @@
-import { ChangeEvent, Component, FormEvent } from 'react';
+import { ChangeEvent, FormEvent, useEffect, useState } from 'react';
 import './searchForm.css';
 import { ErrorButton } from '../ErrorButton';
 
-type IProps = {
+type ISearchFormProps = {
   onSubmit: (search: string) => void;
 };
 
-type IState = {
-  search: string;
-};
+export function SearchForm({ onSubmit }: ISearchFormProps) {
+  const [search, setSearch] = useState<string>('');
 
-export class SearchForm extends Component<IProps, IState> {
-  constructor(props: IProps) {
-    super(props);
-    this.state = {
-      search: '',
-    };
-  }
-
-  componentDidMount() {
-    const { onSubmit } = this.props;
-    const search = localStorage.getItem('search');
-
-    this.setState({
-      search: search || '',
-    });
-
-    onSubmit(search || '');
-  }
-
-  handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-    this.setState({
-      search: event.target.value,
-    });
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setSearch(event.target.value);
   };
 
-  handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-
-    const { search } = this.state;
-    const { onSubmit } = this.props;
 
     localStorage.setItem('search', search);
 
     onSubmit(search);
   };
 
-  render() {
-    const { search } = this.state;
+  useEffect(() => {
+    const searchLS = localStorage.getItem('search');
 
-    return (
-      <form className="form" onSubmit={this.handleSubmit}>
-        <label className="label" htmlFor="search">
-          <input
-            className="input"
-            type="text"
-            id="search"
-            name="search"
-            value={search}
-            onChange={this.handleChange}
-            placeholder="May the force be with you"
-          />
-        </label>
-        <button type="submit">search</button>
-        <ErrorButton />
-      </form>
-    );
-  }
+    setSearch(searchLS || '');
+
+    onSubmit(searchLS || '');
+  }, [onSubmit]);
+
+  return (
+    <form className="form" onSubmit={handleSubmit}>
+      <label className="label" htmlFor="search">
+        <input
+          className="input"
+          type="text"
+          id="search"
+          name="search"
+          value={search}
+          onChange={handleChange}
+          placeholder="May the force be with you"
+        />
+      </label>
+      <button type="submit">search</button>
+      <ErrorButton />
+    </form>
+  );
 }
